083b3f9cdb383a5a7e2253983495d3cb4e879a3c
kikiumorra2 <kikiumorra2@gmail.com>
2026-09-22 17:03:55 -0400

    Update config.js prolific code correct

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
   experimentName: "SAP_mousetrack_v1_388",
 
   // Completion code shown on the final screen. Must match completionUrl in magpie.config.js.
-  completionCode: "C1B0Y2VH", 
+  completionCode: "CCH6PRPT", 
 
   // Which list to use when the URL has no ?LIST_ID=N parameter.
   // "random" picks uniformly among materials/lists/list_*.csv; a number picks that list.
